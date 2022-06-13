dc8e7ac078ada2e93c7a24978eaa3b260a368d34
DeanSupertramp <andry_cz@hotmail.it>
2022-06-13 18:09:48 +0200

    correct spice model, add meas script, fix and upgrade python code

---
+++ utils/script/logSpice2Matrix.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 14:48:08 2022
+
+@author: alecce
+"""
+import pandas as pd
+
+path = "/home/alecce/Scrivania/Test_TiePie/repo/pyTiePie/utils/Spice_Circuit/RC_Difference_LTSpice.log"
+lunghezza = 100
+
+def getMatrix():
+    try:
+        with open(path) as file:
+            lines = file.readlines()
+           # lines = [line.rstrip() for line in lines]
+            
+        for l in range(len(lines)):
+            if lines[l].find("vdiff") != -1:
+                index = l
+        
+        df_list = []
+        for l in range(index+2,index+102):
+            df = pd.DataFrame([lines[l].split('\t')])
+            df_list.append(df)
+        
+        final_df = pd.concat(df_list)
+        new=final_df[1].values.reshape(10,10).T
+        
+        file.close()
+    except:
+        print("errore apertura log Spice")
+    
+    return new
+
+if __name__ == '__main__':
+    matrice = getMatrix()
+    
--- utils/script/script.py
+++ utils/script/script.py
@@ -1,8 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+import logSpice2Matrix as S2M
+
 nstep = 10
 dCstep = 100
+init_dCstep = 100
 
 f = 55000.0
 w = 2*np.pi*f
@@ -11,32 +14,28 @@ dC = C/dCstep
 R = 1/(w*C)
 Vin = 10
 
-
 C_value = np.zeros(nstep);
-#C_value = C_value.reshape(1,-1)
 
-dC_value = np.zeros([nstep,dCstep]);
+dC_value = np.zeros([nstep,nstep]);
 
-V1_num_m = np.zeros([nstep,dCstep]);
+V1_num_m = np.zeros([nstep,nstep]);
 V2_num_m = np.zeros(nstep);
 
-V1_den_m = np.zeros([nstep,dCstep]);
+V1_den_m = np.zeros([nstep,nstep]);
 V2_den_m = np.zeros(nstep);
 
-V1_m = np.zeros([nstep,dCstep]);
+V1_m = np.zeros([nstep,nstep]);
 V2_m = np.zeros(nstep);
 
-diff_m = np.zeros([nstep,dCstep]);
+diff_m = np.zeros([nstep,nstep]);
 ii = np.arange(nstep);
 
-num = complex(0,w*R*C)
-den = complex(1, w*R*C)
-FdT = num/den
+matr = np.zeros([10,10])
 
 
 def ciclo():
     for i in range(1,nstep+1):
-        for j in range(1,dCstep+1):
+        for j in range(1,nstep+1):
             C_value[i-1]=C*i;                           # da 1pF a 10pF
             dC_value[i-1,j-1] = C_value[i-1]/(j*dCstep)    # da C/100 a C/1000
 
@@ -48,17 +47,17 @@ def voltageDifference():
     V2_num = w * R * C;
     V2_den = np.sqrt(1 + pow(w,2) * pow(R,2) * pow(C,2));
     V2 = Vin * V2_num /V2_den;
-    diff = (V1-V2)*1000;    # mVs
+    diff = (V1-V2);    
     print("V1 = " + str(V1) + " V")
     print("V2 = " + str(V2) + " V")
-    print("V1 - V2 = " + str(diff) + " mV")
+    print("V1 - V2 = " + str(diff) + " V")
     out = {'V1_num':V1_num, 'V1_den':V1_den, 'V1':V1, 'V2_num':V2_num, 'V2_den':V2_den, 'V2':V2, 'diff':diff}
     return out
 
 
 def voltageDifference_CICLO():
     for i in range(nstep):
-        for j in range(dCstep):
+        for j in range(nstep):
 
             V1_num_m[i,j] = w * R * (C_value[i] + dC_value[i,j]);
             V1_den_m[i,j] = np.sqrt(1 + pow(w,2) * pow(R,2) * pow(C_value[i]+dC_value[i,j],2));
@@ -66,14 +65,14 @@ def voltageDifference_CICLO():
             V2_num_m[i] = w * R * C_value[i];
             V2_den_m[i] = np.sqrt(1 + pow(w,2) * pow(R,2) * pow(C_value[i],2));
             V2_m[i] = Vin * V2_num_m[i] /V2_den_m[i];
-            diff_m[i,j] = (V1_m[i,j]-V2_m[i])*1000;    # mVs
+            diff_m[i,j] = (V1_m[i,j]-V2_m[i]);    
             
             
 def plotGraph():
     plt.figure()
     fig, axs = plt.subplots(2)
-    fig.suptitle('Voltage measurements ')
-    axs[0].plot(ii[1:], V1_m[1:,dCstep-1])
+    fig.suptitle('Voltage measurements')
+    axs[0].plot(ii[1:], V1_m[1:,nstep-1])
     axs[1].plot(ii[1:], V2_m[1:])
     axs[0].set_title('V1')
     axs[1].set_title('V2')
@@ -83,10 +82,9 @@ def plotGraph():
 
     plt.figure()
     plt.title("Theorical Voltage Difference")
-    plt.ylabel('Voltage Difference [mV]')
+    plt.ylabel('Voltage Difference [V]')
     plt.xlabel('n steps')
     plt.plot(ii[:], diff_m[:,:])
-    #plt.legend(loc='upper right')
     
     plt.figure()
     plt.title("Theorical Voltage Difference Map")
@@ -96,12 +94,36 @@ def plotGraph():
                extent=[dCstep, dCstep*nstep, nstep, 1],
                aspect='auto')
     plt.colorbar()
-
-
+    
+    plt.figure()
+    plt.title("Theorical Voltage Difference Map Spice")
+    plt.ylabel('Cap Value [pF]')
+    plt.xlabel('dC')
+    plt.imshow(matr, origin='upper',
+               extent=[dCstep, dCstep*nstep, nstep, 1],
+               aspect='auto')
+    plt.colorbar()
+    
+    plt.figure()
+    plt.title("Spice vs. Simulated Voltage Error")
+    plt.ylabel('Cap Value [pF]')
+    plt.xlabel('dC')
+    plt.imshow(error, #origin='upper',
+               extent=[dCstep, dCstep*nstep, nstep, 1],
+               aspect='auto')
+    plt.colorbar()
+    
 
 if __name__ == '__main__':
     o = voltageDifference()
     ciclo()
     voltageDifference_CICLO()
+    
+    matr = S2M.getMatrix().astype(np.float) # valori simulati su Spice
+    
+    error = (diff_m - matr)
+    
+    mse = np.mean((diff_m - matr)**2)     # Mean Square Error
+
     plotGraph()
 
